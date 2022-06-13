3950c5ac1617c556a21da9884ed4174f619738fe
Taylove <2294694766@qq.com>
2022-06-13 20:53:27 +0800

    first modify from hot_fix

--- src/main/java/com/qfn/git/TestGit.java
+++ src/main/java/com/qfn/git/TestGit.java
@@ -9,5 +9,6 @@ public class TestGit {
         System.out.println("Hello Git!");
         System.out.println("Hello Git2!");
         System.out.println("Hello Git3!");
+        System.out.println("Hello Git4 from hot_fix");
     }
 }
