6febd7c4c3e7db9c4cbe619fd695d35fd2d82c47
Taylove <2294694766@qq.com>
2022-06-13 21:10:34 +0800

    Hello remote

--- src/main/java/com/qfn/git/TestGit.java
+++ src/main/java/com/qfn/git/TestGit.java
@@ -12,5 +12,6 @@ public class TestGit {
         System.out.println("Hello Git4 from hot_fix");
         System.out.println("Hello Git5!");
         System.out.println("Hello Git5 from hot_fix");
+        System.out.println("Hello remote!");
     }
 }
