8b40c97fa0aed8eb935c38ebbaf4b81e85267358
Taylove <2294694766@qq.com>
2022-06-13 20:42:41 +0800

    first commit from idea

---
+++ src/main/java/com/qfn/git/TestGit.java
@@ -0,0 +1,11 @@
+package com.qfn.git;
+
+/**
+ * @author QFN
+ * @create 2022-06-13 20:39
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
